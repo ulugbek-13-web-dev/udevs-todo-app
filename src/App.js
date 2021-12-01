c80b9d6f1b5fb859398d05d5624f807677399d67
unknown <u.davronov.1999@gmail.com>
2021-12-01 09:11:13 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import AppHeader from './components/appHeader/appHeader'
+import SearchPanel from './components/searchPanel/searchPanel';
+import FilterPanel from './components/filterPanel/filterPanel';
+import TodoList from './components/todoList/todoList'
+import TodoAddForm from './components/todoAddForm/todoAddForm';
+import { useState } from 'react';
+
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      label: 'Drink Coffee',
+      important: false,
+      done: false,
+      id: 1
+    },
+    {
+      label: 'Make Awesome App',
+      important: false,
+      done: false,
+      id: 2
+    },
+    {
+      label: 'Have a lunchs',
+      important: false,
+      done: false,
+      id: 3
+    }
+  ])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppHeader />
+      <div className="search-and-filter">
+        <div className="search-and-filter-cont">
+          <SearchPanel />
+          <FilterPanel />
+        </div>
+
+      </div>
+      <TodoList todos={todos} />
+      <TodoAddForm />
     </div>
   );
 }
